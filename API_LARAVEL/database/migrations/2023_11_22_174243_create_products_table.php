1079513b2f11431cdc98e454e6bd72e588431772
Dario Contreras <thedaroo81@gmail.com>
2023-11-22 14:51:36 -0300

    Commit RU 14:51

--- API_LARAVEL/database/migrations/2023_11_22_174243_create_products_table.php
+++ API_LARAVEL/database/migrations/2023_11_22_174243_create_products_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            
             $table->int('price');
             $table->text('description')->nullable();
             $table->string('image');
             $table->timestamps();
+
         });
     }
 
